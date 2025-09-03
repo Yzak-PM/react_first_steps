6cb4e16e0ea4f4f82cfaf1f0f818b19bf01f8a3e
Yzak-PM <yzakpm@gmail.com>
2025-09-03 16:33:16 -0600

    Funciones del juego del gato completadas

--- 01-tic-tac-toe/src/App.jsx
+++ 01-tic-tac-toe/src/App.jsx
@@ -1,44 +1,63 @@
 import { useState } from 'react'
+import { Square } from './components/Square'
+import { TURNS } from './constants.js'
+import { checkWinner } from './logic/board.js'
+import { WinnerTxt } from './components/WinnerTxt.jsx'
 import './App.css'
-
-
-//! Objeto
-const TURNS = {
-  X: 'x',
-  O: 'o'
-}
-
-const Square = ({ children, isSelected, updateBoard, index }) => {
-  const className = `square ${isSelected ? 'is-selected' : ''}`
-  
-  const handleClick = () =>{
-    updateBoard(index)
-  }
-
-  return(
-    <div onClick={handleClick} className={className}>
-      {children}
-    </div>
-  )
-}
 
 function App() {
   //* Primera posicion = valor actual del estado
   //* 2da posicion = funcion que actualiza el estado
-  const [board, setBoard] = useState(Array(9).fill(null))
+  const [board, setBoard] = useState(() => {
+    const boardFromStorage = window.localStorage.getItem('board')
+    return boardFromStorage ? JSON.parse(boardFromStorage) : Array(9).fill(null)
+  })
 
-  const [turn, setTurn] = useState(TURNS.X)
+  const [turn, setTurn] = useState(() => {
+    const turnFromStorage = window.localStorage.getItem('turn')
+
+    return turnFromStorage ?? TURNS.X
+  })
+
+  const [winner, setWinner] = useState(null)
+
+  const resetGame = () => {
+    setBoard(Array(9).fill(null))
+    setTurn(TURNS.X)
+    setWinner(null)
+
+    window.localStorage.removeItem('board')
+    window.localStorage.removeItem('turn')
+  }
+
+  const checkEndGame = (checkBoard) => {
+    return checkBoard.every((square) => square !== null)
+  }
 
   const updateBoard = (index) => {
-    //todo spread y rest operator
+    if(board[index] || winner) return //@ Si en el board que se dio click ya hay algo, no hacer nada
+
+    //@ Actualizar el tablero
     const newBoard = [... board]
     newBoard[index] = turn
+    setBoard(newBoard)
 
+    //@ Cambiar el turno
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
     setTurn(newTurn)
 
-    setBoard(newBoard)
 
+    //@ guardar partida
+    window.localStorage.setItem('board', JSON.stringify(newBoard))
+    window.localStorage.setItem('turn', newTurn)
+
+    //@ Ver si hay un ganador
+    const newWinner = checkWinner(newBoard)
+    if(newWinner){
+      setWinner(newWinner)
+    }else if(checkEndGame(newBoard)){
+      setWinner(false) //  empate
+    }
   }
 
   return (
@@ -47,6 +66,7 @@ function App() {
 
       <section className='game'>
         {
+          //& _ indica la primera posicion
           board.map((_, index) => {
             return(
               <Square 
@@ -66,6 +86,13 @@ function App() {
         <Square isSelected={turn === TURNS.X}>{TURNS.X}</Square>
         <Square isSelected={turn === TURNS.O}>{TURNS.O}</Square>
       </section>
+
+      <WinnerTxt winner={winner}/>
+
+      <section>
+        <button onClick={resetGame}>Empezar de nuevo</button>
+      </section>
+
     </main>
   )
 }
